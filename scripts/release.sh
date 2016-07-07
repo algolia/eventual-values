5efe8da89610383d7a798fd73322ab525702f71b
Alexandre Meunier <alexandre.meunier@algolia.com>
2016-07-07 17:37:53 +0200

    Fixed release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -70,14 +70,9 @@ git push origin --tags
 # We are gonna publish the package to npm, in a way
 # where only the dist cdn and npm are available
 cp package.json README.md LICENSE dist/
-# jsDelivr original structure for places.js
-# cannot be changed easily: https://github.com/jsdelivr/jsdelivr/issues/12282
 mkdir dist/dist
-mv dist/cdn dist/dist
 cd dist
 npm publish
-mv dist/cdn cdn
-rm dist -rf
 rm package.json README.md LICENSE
 cd ..
 
